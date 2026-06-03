4546f74ef09850ebba8381a5743b71f06ce8505d
Bereket Kiros <bereketkiros17@gmail.com>
2026-06-03 09:12:20 +0300

    Build and style footer component with responsive behavior

--- netflix-clone/src/components/footer/Footer.jsx
+++ netflix-clone/src/components/footer/Footer.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-     
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <FacebookIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investigator Notice</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy-write">
+          &copy; 1997-2026 Netflix, Inc.
+        </div>
+      </div>
     </div>
-  )
+  );
 }
